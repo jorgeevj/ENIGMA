176c9639368eaf5d0b11c103808911301d0b3fd1
jorge villavicencio <jorgeevj@gmail.com>
2015-11-06 19:42:30 -0500

    Atributos del Disco JORGE

--- src/controles/disco.java
+++ src/controles/disco.java
@@ -10,5 +10,40 @@ package controles;
  * @author Jorge
  */
 public class disco {
-    
+  private String discoInterno;
+  private String discoExterno;
+  private int posicionActual;
+  private int posicionInicial;
+
+    public String getDiscoInterno() {
+        return discoInterno;
+    }
+
+    public void setDiscoInterno(String discoInterno) {
+        this.discoInterno = discoInterno;
+    }
+
+    public String getDiscoExterno() {
+        return discoExterno;
+    }
+
+    public void setDiscoExterno(String discoExterno) {
+        this.discoExterno = discoExterno;
+    }
+
+    public int getPosicionActual() {
+        return posicionActual;
+    }
+
+    public void setPosicionActual(int posicionActual) {
+        this.posicionActual = posicionActual;
+    }
+
+    public int getPosicionInicial() {
+        return posicionInicial;
+    }
+
+    public void setPosicionInicial(int posicionInicial) {
+        this.posicionInicial = posicionInicial;
+    }
 }
